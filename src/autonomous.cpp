bbef6184aa0952e2f7c22e8af3daac55912f8b6a
Emanon <emanon@localhost.localdomain>
2019-12-02 07:04:01 -0500

    Add new routines for testing

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -15,24 +15,41 @@ void autonomous() {
 
   // Autonomous phase
   switch (auton) {
-    case 1: {
+    case 1: { /* TOP RED AUTONOMOUS */
               controller.print(2,0, "Run: [TOP RED]");
               break;
             }
-    case 2: {
+    case 2: { /* BOTTOM RED AUTONOMOUS */
               controller.print(2,0, "Run: [BOT RED]");
+              Intake.forward(1);
+              Drive.moveDistance(4.0_ft);
+              // Drive.moveDistance(-3.0_ft);
+              // Drive.turnAngle(45.0_deg);
+              // Drive.moveDistance(-2.0_ft);
+              // Drive.turnAngle(-45.0_deg);
+              // Drive.moveDistance(5.0_ft);
+              // Drive.moveDistance(-5.0_ft);
               break;
             }
-    case 3: {
+    case 3: { /* TOP BLUE AUTONOMOUS */
               controller.print(2,0, "Run: [TOP BLUE]");
               break;
             }
-    case 4: {
+    case 4: { /* BOTTOM BLUE AUTONOMOUS */
               controller.print(2,0, "Run: [BOT BLUE]");
+              Intake.forward(1);
+              Drive.moveDistance(4.0_ft);
+              // Drive.moveDistance(-3.0_ft);
+              // Drive.turnAngle(-45.0_deg);
+              // Drive.moveDistance(-2.0_ft);
+              // Drive.turnAngle(45.0_deg);
+              // Drive.moveDistance(5.0_ft);
+              // Drive.moveDistance(-5.0_ft);
               break;
             }
-    case 5: {
-              controller.print(2,0, "Run: [Skills]");
+    case 5: { /* SKILL AUTONOMOUS */
+              controller.print(2,0, "Run: [SKILLS]");
+              Intake.forward(1);
               break;
             }
   }
