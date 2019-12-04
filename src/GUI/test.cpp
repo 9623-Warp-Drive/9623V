afe44d58a0d0f20658194b4adce36a6b574ba21d
Emanon <emanon@localhost.localdomain>
2019-12-04 16:45:35 -0500

    Add new parameters

--- src/GUI/test.cpp
+++ src/GUI/test.cpp
@@ -2,12 +2,14 @@
 
 void test() {
   setBackgroundColor();
-  createLine(lv_scr_act(), center_separator, 2);
-  createLine(lv_scr_act(), red_side, 7);
-  createLine(lv_scr_act(), blue_side, 7);
-  createBtn(lv_scr_act(), 55, 40, btn_width, btn_height, 1, "TOP RED");
-  createBtn(lv_scr_act(), 55, 235, btn_width, btn_height, 2, "BOT RED");
-  createBtn(lv_scr_act(), 217, 40, btn_width, btn_height, 1, "TOP BLUE");
-  createBtn(lv_scr_act(), 217, 235, btn_width, btn_height, 2, "BOT BLUE");
-  createBtn(lv_scr_act(), 140, 136, btn_width, btn_height, 5, "SKILL");
+
+  createLine(centerSeperator, lv_scr_act(), center_separator, 2);
+  createLine(redOutline, lv_scr_act(), red_side, 7);
+  createLine(blueOutline, lv_scr_act(), blue_side, 7);
+
+  createBtn(topRedBtn, lv_scr_act(), 55, 40, btn_width, btn_height, 1, "TOP RED");
+  createBtn(botRedBtn, lv_scr_act(), 55, 235, btn_width, btn_height, 2, "BOT RED");
+  createBtn(topBlueBtn, lv_scr_act(), 217, 40, btn_width, btn_height, 1, "TOP BLUE");
+  createBtn(botBlueBtn, lv_scr_act(), 217, 235, btn_width, btn_height, 2, "BOT BLUE");
+  createBtn(skillBtn, lv_scr_act(), 140, 136, btn_width, btn_height, 5, "SKILL");
 }
