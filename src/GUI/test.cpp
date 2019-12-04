a62d96e9e33de17260b6d4c4ece063ea272b9bd1
Emanon <emanon@localhost.localdomain>
2019-12-04 15:28:13 -0500

    Add functions to test

--- src/GUI/test.cpp
+++ src/GUI/test.cpp
@@ -1,2 +1,12 @@
 #include "main.h"
-void test() {}
+void test() {
+  setBackgroundColor();
+  createLine(lv_scr_act(), center_separator, 2);
+  createLine(lv_scr_act(), red_side, 7);
+  createLine(lv_scr_act(), blue_side, 7);
+  createBtn(lv_scr_act(), 55, 40, btn_width, btn_height, 1, "TOP RED");
+  createBtn(lv_scr_act(), 55, 235, btn_width, btn_height, 2, "BOT RED");
+  createBtn(lv_scr_act(), 217, 40, btn_width, btn_height, 1, "TOP BLUE");
+  createBtn(lv_scr_act(), 217, 235, btn_width, btn_height, 2, "BOT BLUE");
+  createBtn(lv_scr_act(), 140, 136, btn_width, btn_height, 5, "SKILL");
+}
