a3236b636d88d0618012a7a872c8f82f4daa0018
Emanon <emanon@localhost.localdomain>
2019-12-04 16:45:12 -0500

    Initialize objects and change parameters

--- include/gui.h
+++ include/gui.h
@@ -1,3 +1,15 @@
+#ifndef OBJECT
+#define OBJECT
+extern lv_obj_t * redOutline;
+extern lv_obj_t * blueOutline;
+extern lv_obj_t * centerSeperator;
+extern lv_obj_t * topRedBtn;
+extern lv_obj_t * botRedBtn;
+extern lv_obj_t * topBlueBtn;
+extern lv_obj_t * botBlueBtn;
+extern lv_obj_t * skillBtn;
+#endif /* ifndef OBJECT */
+
 #ifndef DISPLAY
 #define DISPLAY
 extern lv_color_t BG;
@@ -10,7 +22,7 @@ extern lv_color_t OUTLINE_COLOR;
 extern lv_point_t center_separator[];
 extern lv_point_t red_side[];
 extern lv_point_t blue_side[];
-extern lv_obj_t * createLine(lv_obj_t * parent, lv_point_t * pointArray, uint8_t arrayNum);
+extern lv_obj_t * createLine(lv_obj_t * line, lv_obj_t * parent, lv_point_t * pointArray, uint8_t arrayNum);
 void setLineStyle(lv_obj_t * line);
 #endif /* ifndef OUTLINE */
 
@@ -20,7 +32,7 @@ extern uint8_t auton;
 void gui(void);
 void groot(void);
 extern lv_res_t btn_click_action(lv_obj_t * btn);
-extern lv_obj_t * createBtn(lv_obj_t * parent, lv_coord_t x, lv_coord_t y, lv_coord_t width, lv_coord_t height, uint8_t id, const char * title);
+extern lv_obj_t * createBtn(lv_obj_t * btn, lv_obj_t * parent, lv_coord_t x, lv_coord_t y, lv_coord_t width, lv_coord_t height, uint8_t id, const char * title);
 extern lv_style_t * createBtnStyle(lv_style_t * copy, lv_color_t rel, lv_color_t pr, lv_color_t textColor, lv_obj_t * btn);
 void setBtnStyle(lv_style_t * btnStyle, lv_obj_t * btn);
 extern lv_coord_t btn_width;
