2292b13e537445b53cbdb965c1871286720b89e0
Emanon <emanon@localhost.localdomain>
2019-12-04 15:29:04 -0500

    Try to be a bit more organize

---
+++ src/initialize/initialize.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+void initialize() {
+  pros::delay(10);
+  gui();
+}
+
+void disabled() {}
+
+void competition_initialize() {}
---
+++ src/initialize/port_config.cpp
@@ -0,0 +1,7 @@
+#include "main.h"
+
+pros::Controller controller (CONTROLLER_MASTER);
+pros::Motor Diff (16, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+pros::Motor Slide (15, MOTOR_GEARSET_36, false, MOTOR_ENCODER_DEGREES);
+pros::Motor Right_Motor (1, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+pros::Motor Left_Motor (10, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
