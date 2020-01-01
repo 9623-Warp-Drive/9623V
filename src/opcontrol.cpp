dd7fba2efe1c9dde44501400df5ec969b608ade9
khue <khuengyrn123456@gmail.com>
2020-01-01 02:04:39 -0500

    Remove bloats

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,9 +1,8 @@
 #include "main.h"
 #include "port_config.hpp"
-#include "binding.hpp"
 
 extern "C" {
-  #include "gui.h"
+#include "gui.h"
 }
 
 void opcontrol() {
@@ -19,13 +18,48 @@ void opcontrol() {
 
   controller.clear_line(2);
 
-  binding b;
-
   while (true) {
-    b.drive();
-    b.lift();
-    b.intake();
-    b.slide();
+
+    /* Set Drive Binding */
+    int8_t power = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int8_t turn = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) / 4;
+    rightMotor.move(power - turn);
+    leftMotor.move(power + turn);
+
+    /* Set Intake Binding */
+    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+      Intake.setMaxVelocity(50);
+      Intake.forward(-1);
+    }
+    else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+      Intake.setMaxVelocity(600);
+      Intake.forward(1);
+    }
+    else {
+      Intake.stop();
+    }
+
+    /* Set Lift Binding */
+    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+      Lift.forward(1);
+    }
+    else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+      Lift.forward(-1);
+    }
+    else {
+      Lift.stop();
+    }
+
+    /* Set Slide Binding */
+    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_X)) {
+      Slide.forward(1);
+    }
+    else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+      Slide.forward(-1);
+    }
+    else {
+      Slide.forward(0);
+    }
 
     pros::delay(1);
   }
