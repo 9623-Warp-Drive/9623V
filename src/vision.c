f7324069fcae8dcd17b79753dbbd708d5c2f6208
khue <khuengyrn123456@gmail.com>
2020-01-04 15:33:57 -0500

    Use a more sane syntax for vision sensor

--- src/vision.c
+++ src/vision.c
@@ -3,15 +3,17 @@
 
 void
 setVisionSig(void) {
-  vision_signature_s_t BLUE_SIG = BLUE_UTIL_SIG;
-  vision_signature_s_t ORANGE_SIG = ORANGE_UTIL_SIG;
-  vision_signature_s_t PURPLE_SIG = PURPLE_UTIL_SIG;
-  vision_signature_s_t BLUE_ZONE_SIG = BLUE_ZONE_UTIL_SIG;
-  vision_signature_s_t RED_ZONE_SIG = RED_ZONE_UTIL_SIG;
+  const char SIG_NUM[5] = { 1, 2, 3, 4, 5 };
 
-  vision_set_signature(VISION_PORT, SIG_1, &BLUE_SIG);
-  vision_set_signature(VISION_PORT, SIG_2, &ORANGE_SIG);
-  vision_set_signature(VISION_PORT, SIG_3, &PURPLE_SIG);
-  vision_set_signature(VISION_PORT, SIG_3, &BLUE_ZONE_SIG);
-  vision_set_signature(VISION_PORT, SIG_3, &RED_ZONE_SIG);
+  vision_signature_s_t BLUE_SIG = vision_signature_from_utility(SIG_NUM[0], 0, 0, 0, 0, 0, 0, 0, 0);
+  vision_signature_s_t ORANGE_SIG = vision_signature_from_utility(SIG_NUM[1], 0, 0, 0, 0, 0, 0, 0, 0);
+  vision_signature_s_t PURPLE_SIG = vision_signature_from_utility(SIG_NUM[2], 0, 0, 0, 0, 0, 0, 0, 0);
+  vision_signature_s_t BLUE_ZONE_SIG = vision_signature_from_utility(SIG_NUM[3], 0, 0, 0, 0, 0, 0, 0, 0);
+  vision_signature_s_t RED_ZONE_SIG = vision_signature_from_utility(SIG_NUM[4], 0, 0, 0, 0, 0, 0, 0, 0);
+
+  vision_set_signature(VISION_PORT, SIG_NUM[0], &BLUE_SIG);
+  vision_set_signature(VISION_PORT, SIG_NUM[1], &ORANGE_SIG);
+  vision_set_signature(VISION_PORT, SIG_NUM[2], &PURPLE_SIG);
+  vision_set_signature(VISION_PORT, SIG_NUM[3], &BLUE_ZONE_SIG);
+  vision_set_signature(VISION_PORT, SIG_NUM[4], &RED_ZONE_SIG);
 }
