0489619b6e57221ae6f96df04dc2fa626e35e719
khue <khuengyrn123456@gmail.com>
2020-01-03 23:54:15 -0500

    Add basic vision initialization

--- include/vision.h
+++ include/vision.h
@@ -1,3 +1,19 @@
 #ifndef _COLOR_SIG_
 #define _COLOR_SIG_
+
+#define VISION_PORT 13
+
+#define SIG_1 1
+#define SIG_2 2
+#define SIG_3 3
+#define SIG_4 4
+#define SIG_5 5
+
+#define BLUE_UTIL_SIG vision_signature_from_utility(SIG_1, 0, 0, 0, 0, 0, 0, 0, 0)
+#define ORANGE_UTIL_SIG vision_signature_from_utility(SIG_2, 0, 0, 0, 0, 0, 0, 0, 0)
+#define PURPLE_UTIL_SIG vision_signature_from_utility(SIG_3, 0, 0, 0, 0, 0, 0, 0, 0)
+#define BLUE_ZONE_UTIL_SIG vision_signature_from_utility(SIG_4, 0, 0, 0, 0, 0, 0, 0, 0)
+#define RED_ZONE_UTIL_SIG vision_signature_from_utility(SIG_5, 0, 0, 0, 0, 0, 0, 0, 0)
+
+void setVisionSig(void);
 #endif /* ifndef COLOR_SIG */
--- src/vision.c
+++ src/vision.c
@@ -1,2 +1,17 @@
 #include "main.h"
 #include "vision.h"
+
+void
+setVisionSig(void) {
+  vision_signature_s_t BLUE_SIG = BLUE_UTIL_SIG;
+  vision_signature_s_t ORANGE_SIG = ORANGE_UTIL_SIG;
+  vision_signature_s_t PURPLE_SIG = PURPLE_UTIL_SIG;
+  vision_signature_s_t BLUE_ZONE_SIG = BLUE_ZONE_UTIL_SIG;
+  vision_signature_s_t RED_ZONE_SIG = RED_ZONE_UTIL_SIG;
+
+  vision_set_signature(VISION_PORT, SIG_1, &BLUE_SIG);
+  vision_set_signature(VISION_PORT, SIG_2, &ORANGE_SIG);
+  vision_set_signature(VISION_PORT, SIG_3, &PURPLE_SIG);
+  vision_set_signature(VISION_PORT, SIG_3, &BLUE_ZONE_SIG);
+  vision_set_signature(VISION_PORT, SIG_3, &RED_ZONE_SIG);
+}
