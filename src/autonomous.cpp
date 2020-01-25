e90520c81a9e37268c1d9dcceda338abf08458f3
khue <khuenguyrn123456@gmail.com>
2020-01-25 17:02:18 -0500

    Delete autonomous routines

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -34,45 +34,18 @@ autonomous(void) {
   Intake.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
   Slide.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
 
-  Intake.setMaxVelocity(600);
-  Slide.setMaxVelocity(200);
 
   switch (auton) {
     case 1: { /* TOP RED AUTONOMOUS */
               break;
             }
     case 2: { /* BOTTOM RED AUTONOMOUS */
-              Drive.setMaxVelocity(120);
-              Lift.setMaxVelocity(600);
-              Lift.moveDistance(80);
-              Lift.moveDistance(-80);
-              Intake.forward(1);
-              Drive.moveDistance(2.8_ft);
-              Intake.forward(0);
-              Drive.setMaxVelocity(600);
-              Drive.turnAngle(142.0_deg);
-              Drive.moveDistance(23.0_in);
-              Slide.moveDistance(325);
-              Drive.moveDistance(-2.0_ft);
               break;
             }
     case 3: { /* TOP BLUE AUTONOMOUS */
               break;
             }
     case 4: { /* BOTTOM BLUE AUTONOMOUS */
-              Drive.setMaxVelocity(120);
-              Lift.setMaxVelocity(600);
-              Lift.moveDistance(80);
-              Lift.moveDistance(-80);
-              Intake.forward(1);
-              Drive.moveDistance(3.0_ft);
-              Intake.forward(0);
-              Drive.setMaxVelocity(600);
-              Drive.turnAngle(-142.0_deg);
-              Drive.moveDistance(23.0_in);
-              Slide.moveDistance(325);
-              Drive.moveDistance(-2.0_ft);
-
               break;
             }
     case 5: { /* SKILL AUTONOMOUS */
