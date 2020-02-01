33563bbbfd0fff298f43570ac0134e64ead94627
khue <khuenguyrn123456@gmail.com>
2020-02-01 17:20:01 -0500

    Add message to stderr and sd file

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -3,10 +3,12 @@
 extern "C" {
   #include "gui.h"
   #include "vision.h"
+  #include "recorder.h"
 }
 
 void
 initialize(void) {
+  initRecorder();
   initVisionSensor();
   setVisionSig();
   gui();
