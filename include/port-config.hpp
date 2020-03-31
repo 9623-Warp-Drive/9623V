e8d50194ff1bbf733b1ffba3805860374194e8d7
khue <khuenguyrn123456@gmail.com>
2020-03-31 00:53:40 -0400

    refine port-config header file

--- include/port-config.hpp
+++ include/port-config.hpp
@@ -6,35 +6,64 @@
 
 using namespace okapi;
 
+const static unsigned char rightMotorPort = 1;
+const static unsigned char leftMotorPort = 10;
+
+const static unsigned char rightIntakePort = 2;
+const static unsigned char leftIntakePort = 9;
+
+const static unsigned char rightLiftPort = 13;
+const static unsigned char leftLiftPort = 20;
+
+const static unsigned char rightTrayPort = 16;
+const static unsigned char leftTrayPort = 15;
+
 static pros::Controller controller(pros::E_CONTROLLER_MASTER);
-static pros::Motor rightMotor(1, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-static pros::Motor leftMotor(10, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
-static pros::Motor rightLift(13, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
-static pros::Motor leftLift(20, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
+static pros::Motor rightMotor(rightMotorPort,
+                              pros::E_MOTOR_GEARSET_36, false,
+                              pros::E_MOTOR_ENCODER_DEGREES
+                              );
 
-static auto drive = ChassisControllerFactory::create (
-         1, -10,
-         AbstractMotor::gearset::red,
-         {4.0_in, 16.0_in}
-         );
+static pros::Motor leftMotor(leftMotorPort,
+                             pros::E_MOTOR_GEARSET_36, true,
+                             pros::E_MOTOR_ENCODER_DEGREES
+                             );
 
-static auto intake = ChassisControllerFactory::create (
-         -2, 9,
-         AbstractMotor::gearset::green
-         );
+static auto
+drive = ChassisControllerFactory::create (
+        rightMotorPort, -leftMotorPort,
+        AbstractMotor::gearset::red,
+        {4.0_in, 16.0_in}
+);
 
-static auto lift = ChassisControllerFactory::create (
-         -13, 20,
-         AbstractMotor::gearset::red
-         );
+static auto
+intake = ChassisControllerFactory::create (
+        -rightIntakePort, leftIntakePort,
+        AbstractMotor::gearset::green
+);
 
-static auto tray = ChassisControllerFactory::create (
-                -16, 15,
-                AbstractMotor::gearset::red
-                );
+static auto
+lift = ChassisControllerFactory::create (
+        -rightLiftPort, leftLiftPort,
+        AbstractMotor::gearset::red
+);
 
-static auto intakeAsync = AsyncControllerFactory::posIntegrated({-2,9});
-static auto liftAsync = AsyncControllerFactory::posIntegrated({-12,19});
-static auto trayAsync = AsyncControllerFactory::posIntegrated({-16,15});
+static auto
+tray = ChassisControllerFactory::create (
+        -rightTrayPort, leftTrayPort,
+        AbstractMotor::gearset::red
+);
+
+static auto intakeAsync = AsyncControllerFactory::posIntegrated(
+        {-rightIntakePort, leftIntakePort}
+);
+
+static auto liftAsync = AsyncControllerFactory::posIntegrated(
+        {-rightLiftPort, leftLiftPort}
+);
+
+static auto trayAsync = AsyncControllerFactory::posIntegrated(
+        {-rightTrayPort, leftTrayPort}
+);
 
 #endif /* ifndef _PORT_CONFIG_ */
