ba17d7194da10df0bd05e2b42c4e661e5f4b4e28
Z5483 <khuenguyrn123456@gmail.com>
2020-04-06 21:45:46 -0400

    refactor: use enum to generate port number for motors

--- src/port-config.hpp
+++ src/port-config.hpp
@@ -6,61 +6,63 @@
 
 using namespace okapi;
 
-#define rightMotorPort  1;
-#define leftMotorPort   10;
-#define rightIntakePort 2;
-#define leftIntakePort  9;
-#define rightLiftPort   13;
-#define leftLiftPort    20;
-#define rightTrayPort   16;
-#define leftTrayPort    15;
+enum motor_port {
+	RIGHT_DRIVE =  1,
+	LEFT_DRIVE  =  10,
+	RIGHT_INTAKE=  2,
+	LEFT_INTAKE =  9,
+	RIGHT_LIFT  =  13,
+	LEFT_LIFT   =  20,
+	RIGHT_TRAY  =  16,
+	LEFT_TRAY   =  15
+};
 
 static pros::Controller controller(pros::E_CONTROLLER_MASTER);
-static pros::Motor rightMotor(rightMotorPort,
+static pros::Motor rightMotor(RIGHT_DRIVE,
 			      pros::E_MOTOR_GEARSET_36, false,
 			      pros::E_MOTOR_ENCODER_DEGREES
 			     );
 
-static pros::Motor leftMotor(leftMotorPort,
+static pros::Motor leftMotor(LEFT_DRIVE,
 			     pros::E_MOTOR_GEARSET_36, true,
 			     pros::E_MOTOR_ENCODER_DEGREES
 			    );
 
 static auto
 drive = ChassisControllerFactory::create (
-					  rightMotorPort, -leftMotorPort,
+					  RIGHT_DRIVE, -LEFT_DRIVE,
 					  AbstractMotor::gearset::red,
 					  {4.0_in, 16.0_in}
 					 );
 
 static auto
 intake = ChassisControllerFactory::create (
-					   -rightIntakePort, leftIntakePort,
+					   -RIGHT_INTAKE, LEFT_INTAKE,
 					   AbstractMotor::gearset::green
 					  );
 
 static auto
 lift = ChassisControllerFactory::create (
-					 -rightLiftPort, leftLiftPort,
+					 -RIGHT_LIFT, LEFT_LIFT,
 					 AbstractMotor::gearset::red
 					);
 
 static auto
 tray = ChassisControllerFactory::create (
-					 -rightTrayPort, leftTrayPort,
+					 -RIGHT_TRAY, LEFT_TRAY,
 					 AbstractMotor::gearset::red
 					);
 
 static auto intakeAsync = AsyncControllerFactory::posIntegrated(
-								{-rightIntakePort, leftIntakePort}
+								{-RIGHT_INTAKE, LEFT_INTAKE}
 							       );
 
 static auto liftAsync = AsyncControllerFactory::posIntegrated(
-							      {-rightLiftPort, leftLiftPort}
+							      {-RIGHT_LIFT, LEFT_LIFT}
 							     );
 
 static auto trayAsync = AsyncControllerFactory::posIntegrated(
-							      {-rightTrayPort, leftTrayPort}
+							      {-RIGHT_TRAY, LEFT_TRAY}
 							     );
 
 #endif /* ifndef _PORT_CONFIG_ */
